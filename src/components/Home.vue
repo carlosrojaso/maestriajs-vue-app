eb4b996a0b2ebb25b0b16f203609dae980a1a30d
Carlos Rojas <carlos.rojas@localhost>
2019-12-27 17:37:47 -0500

    adding api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,11 +6,11 @@
         <v-list-item-subtitle>{{note.description}}</v-list-item-subtitle>
       </v-list-item-content>
       <v-list-item-action>
-        <v-btn @click="newNote()" icon>
+        <v-btn @click="getNote(note)" icon>
           <v-icon color="grey lighten-1">mdi-pencil</v-icon>
         </v-btn>
         <v-btn icon>
-          <v-icon color="grey lighten-1">mdi-delete</v-icon>
+          <v-icon @click="deleteNote(note.id)" color="grey lighten-1">mdi-delete</v-icon>
         </v-btn>
       </v-list-item-action>
     </v-list-item>
@@ -36,7 +36,7 @@
           <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="blue darken-1" @click="closeModal()">Close</v-btn>
-              <v-btn color="blue darken-1" @click="saveNote()">Save</v-btn>
+              <v-btn color="blue darken-1" :disabled="!valid" @click="saveNote()">{{(isEditing ? 'Edit': 'Save')}}</v-btn>
           </v-card-actions>
         </v-form>
         </v-card>
@@ -45,16 +45,19 @@
 </template>
 
 <script>
-import {DummyData} from "../data/dummy-data";
+import {notesDataApi} from "../data/notes-data-api";
 
 export default {
   name: 'Home',
+  mixins: [notesDataApi],
   data: () => ({  
-    notes: DummyData,
+    notes: [],
     dialog: false,
+    isEditing: false,
+    idToEdit: -1,
     newTitle: '',
     newContent: '',
-    valid: false,
+    valid: true,
     nameRules: [
         v => !!v || 'Name is required',
         v => v.length >= 4 || 'Name must be at least 4 characters',
@@ -66,14 +69,78 @@ export default {
   }),
   mounted() {
     this.$root.$on('NEW_NOTE', (action) => {if (action) { this.newNote(); }});
+    this.getTasks(1)
+      .then((res) => res.json())
+      .then((items) => {
+        this.notes = items.map((item) => ({id: item.id, name: item.title, description: item.body}));
+        });
   },
   methods: {
+    deleteNote(id) {
+      const noteToDelete = this.notes.findIndex((item) => (item.id === id));
+      this.deleteTask(id)
+        .then(res => res.json())
+        .then(
+          () => {
+            this.notes.splice(noteToDelete, 1);
+          }
+        );
+    },
+    saveNote() {
+      if (!this.isEditing) { 
+        const noteToSave = {
+          id: this.notes.length + 1,
+          name: this.newTitle,
+          description: this.newContent
+          };
+
+      this.createTask(noteToSave)
+        .then(res => res.json())
+        .then(
+          () => {
+            this.notes.push(noteToSave);
+            this.closeModal();
+          }
+        );
+      } else {
+        const originalNote = this.notes.findIndex((item) => (item.id === this.idToEdit));
+        const noteToEdit = {
+          id: this.idToEdit,
+          name: this.newTitle,
+          description: this.newContent
+          };
+        this.putTask(noteToEdit)
+          .then(res => res.json())
+          .then(
+            () => {
+              this.notes[originalNote] = noteToEdit;
+              this.closeModal();
+            }
+          );
+      }
+
+      this.reset();
+    },
+    getNote(note) {
+      this.idToEdit = note.id;
+      this.newTitle = note.name;
+      this.newContent = note.description;
+      this.isEditing = true;
+      this.dialog = true;
+    },
     newNote () {
       this.dialog = true;
     },
     closeModal () {
       this.dialog = false;
+      this.reset();
     },
+    reset() {
+      this.isEditing = false;
+      this.newTitle = '';
+      this.newContent = '';
+      this.idToEdit = -1;
+    }
   }
 }
 </script>
